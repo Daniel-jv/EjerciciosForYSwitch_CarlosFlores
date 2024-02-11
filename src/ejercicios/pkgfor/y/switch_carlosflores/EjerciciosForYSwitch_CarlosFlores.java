b3efc4a248ba982673805536c2eb71c4c8bd5b45
Daniel-jv <carlosfloreseagle2020@gmail.com>
2024-02-11 11:44:24 -0600

    1

---
+++ src/ejercicios/pkgfor/y/switch_carlosflores/EjerciciosForYSwitch_CarlosFlores.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios.pkgfor.y.switch_carlosflores;
+
+/**
+ *
+ * @author carlo
+ */
+public class EjerciciosForYSwitch_CarlosFlores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
